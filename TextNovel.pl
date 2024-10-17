96e54371cbe2e19c1d340ce784335cf38a15e45f
Pegah Tajvidi <pegahtajvidi82@gmail.com>
2024-10-16 18:44:13 -0700

    TestingBranch

--- TextNovel.pl
+++ TextNovel.pl
@@ -8,5 +8,6 @@ use Thief;
 # figure out how to pass array as argument (so tools could be array of elements)
 
 my $newTheif = Thief->new(50, "Forgery", "None", 100, 20, 0); # creates instance of Thief class
+#testing branch making
 my $newStory = Story->new($newTheif, "Entin"); # creates instance of Story class
 $newStory->startStory(); # begins text adventure
