48592623290e32789d00c44eefdef6b2f8ef8d2d
E-A-H62 <85127520+E-A-H62@users.noreply.github.com>
2024-11-15 15:51:39 -0800

    Update TextNovel.pl

--- TextNovel.pl
+++ TextNovel.pl
@@ -5,8 +5,6 @@ use lib '.';
 use Story;
 use Thief;
 
-# figure out how to pass array as argument (so tools could be array of elements)
-
 my $newThief = Thief->new(50, 100, 20, 0); # creates instance of Thief class
 my $newStory = Story->new($newThief, "Entin"); # creates instance of Story class
 $newStory->startStory(); # begins text adventure
